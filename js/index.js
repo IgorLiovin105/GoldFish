ad9b215cf5f2af695e899d8b07a5adc3b4ea269e
IgorLiovin105 <igorliovin07@gmail.com>
2022-10-30 12:48:13 +0600

    menu

--- js/index.js
+++ js/index.js
@@ -1,19 +1,33 @@
-$('#open-popup-btn').click((e) => {
+$('#open-popup-btn').click(function (e) {
 	e.preventDefault()
 	$('#popup').removeClass('hidden')
 	$('body').addClass('overflow-hidden')
 })
-$('#close-popup-btn').click((e) => {
+$('#close-popup-btn').click(function (e) {
 	e.preventDefault()
 	$('#popup').addClass('hidden')
 	$('body').removeClass('overflow-hidden')
-	$('.info').addClass('hidden')
+	$('#popup-content').addClass('hidden')
 	$('.cat-link').removeClass('bg-orange-400')
+	$('.cat-img').attr('src', 'img/right-def.svg')
 })
 $('.cat-link').click(function (e) {
 	e.preventDefault()
-	$('.info').addClass('hidden')
 	$('.cat-link').removeClass('bg-orange-400')
-	$($(this).attr('href')).removeClass('hidden')
+	$('.cat-img').attr('src', 'img/right-def.svg')
+	$('#popup-content').removeClass('hidden')
 	$(this).addClass('bg-orange-400')
+	$(this).children('.cat-img').attr('src', 'img/right.svg')
+})
+$('.popup-mobile__link').click(function (e) {
+	e.preventDefault()
+	$('#popup-mobile').addClass('hidden')
+	$('#mobile-header__icon').children('img').attr('src', 'img/burger.svg')
+})
+$('#mobile-header__icon').click(function () {
+	$('#popup-mobile').toggleClass('hidden')
+	if ($('#popup-mobile').hasClass('hidden')) {
+		return $(this).children('img').attr('src', 'img/burger.svg')
+	}
+	$(this).children('img').attr('src', 'img/burger-open.svg')
 })
